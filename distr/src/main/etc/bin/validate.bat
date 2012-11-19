0ba5d4980b45e0269449e0c76372e35e72cfbc22
Igor Rodchenkov <rodche@gmail.com>
2012-11-19 15:56:22 -0500

    Fixed script bugs; added a resource file.

--- distr/src/main/etc/bin/validate.bat
+++ distr/src/main/etc/bin/validate.bat
@@ -4,5 +4,5 @@
 set VALIDATOR_OPTS="-javaagent:spring-instrument.jar -Xms2g -Xmx2g -Dfile.encoding=UTF-8 -Djava.io.tmpdir=tmp"
 
 # to run using log4j and obo properties from the working (current) dir -
-java -cp .;validate.jar %VALIDATOR_OPTS% org.biopax.validator.Main $1 $2 $3 $4 $5 $6
+java -cp .;validate.jar %VALIDATOR_OPTS% org.biopax.validator.Main "$1" "$2" "$3" "$4" "$5" "$6"
 
--- distr/src/main/etc/bin/validate.sh
+++ distr/src/main/etc/bin/validate.sh
@@ -6,7 +6,7 @@
 VALIDATOR_OPTS="-javaagent:spring-instrument.jar -Xms2g -Xmx2g -Dfile.encoding=UTF-8 -Djava.io.tmpdir=tmp"
 
 # to run using log4j and obo properties from the working (current) dir -
-$JAVA_HOME/bin/java -cp .:validate.jar $VALIDATOR_OPTS org.biopax.validator.Main $1 $2 $3 $4 $5 $6
+$JAVA_HOME/bin/java -cp .:validate.jar $VALIDATOR_OPTS org.biopax.validator.Main "$1" "$2" "$3" "$4" "$5" "$6"
 
 # alternatively (and as a fall-back), run with default logging and OBO properties (from validate.jar classpath)
-#$JAVA_HOME/bin/java $VALIDATOR_OPTS -jar validate.jar $1 $2 $3 $4 $5 $6
+#$JAVA_HOME/bin/java $VALIDATOR_OPTS -jar validate.jar "$1" "$2" "$3" "$4" "$5" "$6"
