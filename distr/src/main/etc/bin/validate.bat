8635ae955354e118e6d314c9eb7d74cb2d34d278
rodche <rodche@gmail.com>
2014-05-06 17:20:03 -0400

    Fixed comment lines in the MS Win batch script

--- distr/src/main/etc/bin/validate.bat
+++ distr/src/main/etc/bin/validate.bat
@@ -1,15 +1,15 @@
 set VALIDATOR_OPTS="-javaagent:lib/spring-instrument-${spring.version}.jar -Xmx2g -Dfile.encoding=UTF-8"
 
-# make sure the right (the one you want) java installation directory 
-# goes before the system java, etc. in your current PATH
+REM make sure the right (the one you want) java installation directory 
+REM goes before the system java, etc. in your current PATH
 
 echo path: %PATH%
 java --version
 
 
 echo Starting BioPAX Validator...
-# run using log4j and obo properties from current dir -
-#java -cp .;biopax-validator.jar %VALIDATOR_OPTS% org.biopax.validator.Main "$1" "$2" "$3" "$4" "$5" "$6"
+REM run using log4j and obo properties from current dir -
+REM java -cp .;biopax-validator.jar %VALIDATOR_OPTS% org.biopax.validator.Main "$1" "$2" "$3" "$4" "$5" "$6"
 
-# run with default logging and OBO properties (from the default classpath)
+REM run with default logging and OBO properties (from the default classpath)
 java %VALIDATOR_OPTS% -jar biopax-validator.jar "$1" "$2" "$3" "$4" "$5" "$6"
