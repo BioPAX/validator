b616e89b877aaa73e1c2e2c275f5e2b619eca2b8
IgorRodchenkov <rodche@gmail.com>
2016-01-04 01:22:17 -0500

    Removed old miriam-lib dependency

--- biopax-validator/src/main/java/org/biopax/validator/utils/XrefHelper.java
+++ biopax-validator/src/main/java/org/biopax/validator/utils/XrefHelper.java
@@ -1,27 +1,5 @@
 package org.biopax.validator.utils;
 
-/*
- * #%L
- * BioPAX Validator
- * %%
- * Copyright (C) 2008 - 2013 University of Toronto (baderlab.org) and Memorial Sloan-Kettering Cancer Center (cbio.mskcc.org)
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 3 of the 
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
- */
-
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.*;
@@ -32,7 +10,7 @@ import org.apache.commons.collections15.collection.CompositeCollection;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import org.biopax.miriam.MiriamLink;
+import org.biopax.paxtools.normalizer.MiriamLink;
 import org.biopax.psidev.ontology_manager.OntologyAccess;
 import org.biopax.psidev.ontology_manager.OntologyTermI;
 import org.biopax.psidev.ontology_manager.impl.OntologyTermImpl;
@@ -229,7 +207,6 @@ public class XrefHelper {
      * The first in the list is the recommended one.
      *
      * @param name case insensitive
-     * @param groups
      * @return set of equivalent database names
      */
     public List<String> getSynonymsForDbName(String name) {
