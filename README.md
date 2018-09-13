c5a7251aa6de301528274890bcca095ad6e08956
IgorRodchenkov <rodche@gmail.com>
2018-09-13 14:41:55 -0400

    Fixes #9.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # The BioPAX Validator
-(code and docs permanently moved from Sourceforge, hg.code.sf.net/p/biopax/validator, to here on GitHub)
+(code and docs permanently moved from Sourceforge, hg.code.sf.net/p/biopax/validator, to GitHub)
 
 BioPAX.org [online BioPAX Validator](http://www.biopax.org/validator/)
 
--- biopax-validator/src/main/java/org/biopax/validator/rules/PathwayMultiOrganismRule.java
+++ biopax-validator/src/main/java/org/biopax/validator/rules/PathwayMultiOrganismRule.java
@@ -1,77 +1,54 @@
 package org.biopax.validator.rules;
 
-/*
- *
- */
-
-import java.util.Collection;
-import java.util.HashSet;
-
-import org.biopax.paxtools.controller.AbstractTraverser;
+import org.biopax.paxtools.controller.Fetcher;
+import org.biopax.paxtools.controller.PathAccessor;
 import org.biopax.paxtools.controller.PropertyEditor;
-import org.biopax.paxtools.util.Filter;
-import org.biopax.paxtools.model.BioPAXElement;
-import org.biopax.paxtools.model.Model;
+import org.biopax.paxtools.controller.SimpleEditorMap;
 import org.biopax.paxtools.model.level3.BioSource;
 import org.biopax.paxtools.model.level3.Pathway;
+import org.biopax.paxtools.util.Filter;
 import org.biopax.validator.api.AbstractRule;
 import org.biopax.validator.api.beans.Validation;
-import org.biopax.paxtools.controller.SimpleEditorMap;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Warn if a pathway and its component have different (not null) 'organism' values.
  * What to do? (ignore, delete this value, or override nested organism properties with pathway's value)
- * 
+ *
  * @author rodche
  */
 @Component
-public class PathwayMultiOrganismRule extends AbstractRule<Pathway> 
-{	
-	private final static Filter<PropertyEditor> filter = new Filter<PropertyEditor>() {
+public class PathwayMultiOrganismRule extends AbstractRule<Pathway> {
+  private final static Filter<PropertyEditor> filter = new Filter<PropertyEditor>() {
 
-		public boolean filter(PropertyEditor editor) {
-			return !"nextStep".equals(editor.getProperty());
-		}
-	};
-	
-    public void check(final Validation validation, final Pathway pathway) {
-    	final Collection<BioPAXElement> organisms = new HashSet<BioPAXElement>();
-    	final BioSource organism = pathway.getOrganism(); // not null - due to the canCheck method!
-    	//but..
-    	if(organism==null) return; // we do not care
-    	
-    	AbstractTraverser runner = new AbstractTraverser(
-    			SimpleEditorMap.L3, filter) 
-    	{
-    		@Override
-			protected void visit(Object value, BioPAXElement parent, 
-					Model model, PropertyEditor editor) 
-    		{
-				if(value instanceof BioSource) {	
-					if(!((BioPAXElement) value).isEquivalent(organism)) {
-						organisms.add((BioPAXElement) value);
-					}
-				} 
-				else if (value instanceof BioPAXElement) {
-					logger.trace("Traverse into " + value + " "
-							+ value.getClass().getSimpleName());
-					traverse((BioPAXElement) value, model);
-				}
-			}
-    	};
-    	
-   		runner.traverse(pathway, null);
-   		
-		if(organisms.size()>0) {
-			error(validation, pathway, "multi.organism.pathway",
-				false, organism, organisms);
-		}
+    public boolean filter(PropertyEditor editor) {
+      //skip for: nextStep (those can be reached via pathwayOrder or pathwayComponent) and evidence (multi-org. is normal there)
+      return !("nextStep".equals(editor.getProperty()) || "evidence".equals(editor.getProperty()));
     }
+  };
 
-    public boolean canCheck(Object thing) {
-        return thing instanceof Pathway
-        	&& ((Pathway)thing).getOrganism() != null;
+  public void check(final Validation validation, final Pathway pathway) {
+    Fetcher fetcher = new Fetcher(SimpleEditorMap.L3, Fetcher.evidenceFilter, Fetcher.nextStepFilter);
+    fetcher.setSkipSubPathways(true);
+    final Set<BioSource> organisms = fetcher.fetch(pathway, BioSource.class);
+    //collect taxonomy IDs (from the BioSource objects that have valid xrefs)
+    PathAccessor accessor = new PathAccessor("BioSource/xref:UnificationXref/id");
+    Set<String> taxIds = accessor.getValueFromBeans(organisms);
+    if (taxIds.size() > 1) {
+      accessor = new PathAccessor("BioSource/name");
+      Set<String> names = new HashSet<String>();
+      for(Object name  : accessor.getValueFromBeans(organisms))
+       names.add(String.valueOf(name).toLowerCase());
+
+      error(validation, pathway, "multi.organism.pathway", false, taxIds, names);
     }
+  }
+
+  public boolean canCheck(Object thing) {
+    return thing instanceof Pathway;
+  }
 
 }
